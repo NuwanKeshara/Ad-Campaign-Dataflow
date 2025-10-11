899139c80a737e1b91efe6409308c178214700e2
NuwanKeshara <nuwankyazh@gmail.com>
2025-10-11 21:08:00 +0300

    add airflow

--- scripts/load_to_postgres.py
+++ scripts/load_to_postgres.py
@@ -16,12 +16,14 @@ logging.basicConfig(
 )
 
 # load environment variables
-load_dotenv()
+load_dotenv("/opt/airflow/AD-CAMPAIGN-DATAFLOW/.env")
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
+# DB_HOST = os.getenv("DB_HOST", "localhost")
+# DB_PORT = os.getenv("DB_PORT", "5432")
+DB_HOST = "postgres"
+DB_PORT = "5432"
 DB_NAME = os.getenv("DB_NAME")
 TABLE_NAME = os.getenv("TABLE_NAME")
 CSV_PATH = os.getenv("CSV_PATH")
